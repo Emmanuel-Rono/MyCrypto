c9ac1e495eaded0ba5b5f0b76dc93a6da69b6acd
Emmanuel-Rono <ronoemmanuel933@gmail.com>
2023-09-27 19:21:41 +0300

    Initial Commit

---
+++ app/src/main/java/com/cryptoApp/mycrypto/presentation/Activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.CryptoApp.mycrypto.presentation.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emmanuel_rono.mycrypto.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
